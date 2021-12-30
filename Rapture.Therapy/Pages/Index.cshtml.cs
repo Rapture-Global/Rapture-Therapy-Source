141c2b328896ea26932dadebce3dedf9bf3fe317
Eamonn Duffy <Eamonn@Duffy.global>
2021-12-30 00:43:21 +0000

    Essentially reversioning of Rapture-Therapy-Source Repository. And some Shenanigans about Microsoft Logging.

--- Rapture.Therapy/Pages/Index.cshtml.cs
+++ Rapture.Therapy/Pages/Index.cshtml.cs
@@ -1,20 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eadent.Common.WebApi.ApiClient;
+using Eadent.Identity.Access;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rapture.Therapy.DataAccess.RaptureTherapy.Repositories;
 
 namespace Rapture.Therapy.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        // Attributes/Properties.
+        private ILogger<IndexModel> Logger { get; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private IEadentUserIdentity EadentUserIdentity { get; }
+
+        private IRaptureTherapyDatabaseVersionsRepository RaptureTherapyDatabaseVersionsRepository { get; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IEadentUserIdentity eadentUserIdentity, IRaptureTherapyDatabaseVersionsRepository raptureTherapyDatabaseVersionsRepository)
         {
-            _logger = logger;
+            Logger = logger;
+
+            EadentUserIdentity = eadentUserIdentity;
+
+            RaptureTherapyDatabaseVersionsRepository = raptureTherapyDatabaseVersionsRepository;
         }
 
-        public void OnGet()
+        private class PostDto
         {
+            public string title { get; set; }
 
+            public string body { get; set; }
+
+            public long userId { get; set; }
+        }
+
+        private async Task ApiClientTests()
+        {
+            using (var apiClient = new ApiClientJson(Logger, "https://jsonplaceholder.typicode.com/"))
+            {
+                var requestDto = new PostDto()
+                {
+                    title = "Eamonn",
+                    body = "Jennifer",
+                    userId = 69
+                };
+
+                var result = await apiClient.PostAsync<PostDto, dynamic>("/posts", requestDto, null);
+            }
+
+            using (var apiClient = new ApiClientJson(Logger, "https://jsonplaceholder.typicode.com/"))
+            {
+                var result = await apiClient.GetAsync<dynamic>("/posts/1", null);
+            }
+
+            using (var apiClient = new ApiClientJson(Logger, "https://jsonplaceholder.typicode.com/"))
+            {
+                var requestDto = new PostDto()
+                {
+                    title = "Eamonn",
+                    body = "Jennifer",
+                    userId = 69
+                };
+
+                var result = await apiClient.PutAsync<PostDto, dynamic>("/posts", requestDto, null);
+            }
+
+            using (var apiClient = new ApiClientJson(Logger, "https://jsonplaceholder.typicode.com/"))
+            {
+                var result = await apiClient.DeleteAsync<dynamic>("/posts/1", null);
+            }
+        }
+
+        public async Task OnGet()
+        {
+#if false
+            var list = RaptureTherapyDatabaseVersionsRepository.GetAll().ToList();
+
+            //var urlEncodedResetToken = WebUtility.UrlEncode(resetToken);
+
+            //var urlDecodedResetToken = WebUtility.UrlDecode(urlEncodedResetToken);
+
+            //if (urlDecodedResetToken != resetToken)
+            //{
+            //    int breakPoint = 1;
+            //}
+
+            var role = Role.User;
+
+            var eMailAddress = "Eamonn.Duffy@Eadent.com";
+
+            var displayName = "Éamonn Duffy";
+
+            var plainTextPassword = "Éamonn Ó Dubhthaigh!";
+
+            var remoteAddressString = HttpHelper.GetRemoteAddress(Request);
+
+            var registeredUserEntity = EadentUserIdentity.RegisterUser(role, eMailAddress, displayName, plainTextPassword, remoteAddressString);
+
+            SignInStatus signInStatusId = SignInStatus.Error;
+
+            UserSessionEntity userSessionEntity = null;
+
+            string userSessionToken = null;
+
+            SignOutStatus signOutStatusId = SignOutStatus.Error;
+
+            (signInStatusId, userSessionEntity) = EadentUserIdentity.SignInUser(eMailAddress, plainTextPassword, remoteAddressString);
+
+            signOutStatusId = EadentUserIdentity.SignOutUser(userSessionEntity.UserSessionToken, remoteAddressString);
+
+            (signInStatusId, userSessionEntity) = EadentUserIdentity.SignInUser(eMailAddress, "Bad Password", remoteAddressString);
+
+            signOutStatusId = EadentUserIdentity.SignOutUser(userSessionEntity.UserSessionToken, remoteAddressString);
+
+            (signInStatusId, userSessionEntity) = EadentUserIdentity.SignInUser("Does.Not.Exist@Eadent.com", "Bad Password", remoteAddressString);
+
+            signOutStatusId = EadentUserIdentity.SignOutUser(userSessionEntity.UserSessionToken, remoteAddressString);
+
+            var passwordResetRequestStatusId = UserPasswordResetRequestStatus.Error;
+
+            string resetToken = null;
+
+            UserEntity userEntity = null;
+
+            (passwordResetRequestStatusId, resetToken, userEntity) = EadentUserIdentity.BeginUserPasswordReset(eMailAddress, remoteAddressString);
+#endif
+
+            //InterimEadentIdentityTests();
+
+            //await ApiClientTests();
         }
     }
 }
--- Rapture.Therapy/Program.cs
+++ Rapture.Therapy/Program.cs
@@ -1,7 +1,55 @@
+using Eadent.Identity.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using NLog.Web;
+using Rapture.Therapy.Configuration;
+using Rapture.Therapy.DataAccess.RaptureTherapy.Databases;
+using Rapture.Therapy.DataAccess.RaptureTherapy.Repositories;
+using Rapture.Therapy.Sessions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Configuration.AddJsonFile("Confidential/EadentIdentity.settings.json", optional: false, reloadOnChange: false);
+builder.Configuration.AddJsonFile("Confidential/RaptureTherapy.settings.json", optional: false, reloadOnChange: false);
+
+// NLog: Setup NLog for Dependency injection
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Host.UseNLog();
+
+var services = builder.Services;
+
+services.AddDistributedMemoryCache();
+
+services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+services.AddHttpContextAccessor();
+
+Eadent.Identity.Startup.ConfigureServices(services, builder.Configuration.GetSection(EadentIdentitySettings.SectionName).Get<EadentIdentitySettings>());
+
+var databaseSettings = builder.Configuration.GetSection(RaptureTherapySettings.SectionName).Get<RaptureTherapySettings>().Database;
+
+string connectionString = $"Server={databaseSettings.DatabaseServer};Database={databaseSettings.DatabaseName};Application Name={databaseSettings.ApplicationName};User Id={databaseSettings.UserName};Password={databaseSettings.Password};";
+
+services.AddDbContext<RaptureTherapyDatabase>(options => options.UseSqlServer(connectionString));
+
+services.AddScoped<IRaptureTherapyDatabase, RaptureTherapyDatabase>();
+
+services.AddTransient<IRaptureTherapyDatabaseVersionsRepository, RaptureTherapyDatabaseVersionsRepository>();
+
+services.AddTransient<IUserSession, UserSession>();
+
+services.AddRazorPages();
+services.AddDirectoryBrowser();
+
 
 var app = builder.Build();
 
@@ -16,9 +64,17 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "Preliminary")),
+    RequestPath = "/Preliminary"
+});
+
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapRazorPages();
 
