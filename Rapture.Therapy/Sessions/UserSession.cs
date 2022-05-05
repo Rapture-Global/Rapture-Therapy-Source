dfa80f35d28dddc602d108788e9008a7863c5d2c
Eamonn Duffy <Eamonn@Duffy.global>
2022-05-05 17:14:16 +0100

    Renamed Role to RoleName.

--- Rapture.Therapy/Sessions/UserSession.cs
+++ Rapture.Therapy/Sessions/UserSession.cs
@@ -24,7 +24,7 @@ namespace Rapture.Therapy.Sessions
             {
                 RoleId = roleEntity.RoleId;
                 RoleLevel = roleEntity.RoleLevel;
-                RoleName = roleEntity.Role;
+                RoleName = roleEntity.RoleName;
             }
         }
 
